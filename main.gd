e670b3eeac65219e41c2c92f8335717267ce18e6
Luke Phan <lu.phan@leapxpert.com>
2025-10-21 12:45:32 +0700

    completing background and music

--- main.gd
+++ main.gd
@@ -6,11 +6,18 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _ready():
 	pass
@@ -39,6 +46,8 @@ func _on_mod_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	
+	$HUD.update_score(score)
 
 
 func _on_score_timer_timeout():
